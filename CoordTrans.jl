041b3d5d4991c1157e05ea47b5ca8f09bd04dac7
kylechanphy <100992446+kylechanphy@users.noreply.github.com>
2023-12-14 00:12:17 +0800

    Update CoordTrans.jl

--- CoordTrans.jl
+++ CoordTrans.jl
@@ -1,13 +1,15 @@
 """
 Translate a vector from Cartesian to Spherical coordinate
 
-return a vector [theta, phi]
-  - theta: polar angle [0, pi]
-  - phi: azimuthal angle in x-y plane [0, 2pi]
+return a vector [r, theta, phi]
+  - r: length of the vector
+  - theta: polar angle [0, pi] from the z-axis to the vector 
+  - phi: azimuthal angle [0, 2pi] from the x-axis to the projection of the vector in x-y plane.
 """
 function cart2sph(vec)
-  theta = acos(vec[3])
-  phi = sign(vec[2])acos(vec[1] / sqrt(vec[1]^2 + vec[2]^2))
+  r = hypot(vec[1], vec[2], vec[3])
+  theta = acos(vec[3] / r)
+  phi = sign(vec[2])acos(vec[1] / hypot(vec[1] + vec[2]))
   
-  return [theta, phi] 
+  return SA[r, theta, phi] 
 end
